1c11c7ca2fd53f526bcd835671ed827ab7eedb1a
Jason <dimensions3@outlook.com>
2025-02-06 15:00:22 -0800

    trying to get add to path working

--- install_xelis.sh
+++ install_xelis.sh
@@ -1,18 +1,52 @@
-#!/bin/bash
+#!/bin/env bash
+set -eo pipefail
 echo "Installing Rust:"
 curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt install cargo -y
+BASE_DIR="${XDG_CONFIG_HOME:-$HOME}"
+LOCAL_DIR="${LOCAL_DIR-"$BASE_DIR/.local"}"
+XELIS_DIR="$LOCAL_DIR/share/blockchain"
 git clone https://github.com/xelis-project/xelis-blockchain
 source ~/.profile
 sudo chown -R test /home/test/xelis-blockchain/
 cd xelis-blockchain
 echo "Building Xelis:"
 cargo build --release
-mkdir -p /home/.local/share/xelis-blockchain
+# Create heavymeta directory and hvym binary if it doesn't exist.
+mkdir -p "$XELIS_DIR"
 
 mv /home/test/xelis-blockchain/target/release/* /home/.local/share/xelis-blockchain/
 echo "export xelis to path:"
-source ~/.bashrc
-export PATH="/home/.local/share/xelis-blockchain/:$PATH"
-echo "Xelis is installed."
-source ~/.bashrc
+# Store the correct profile file (i.e. .profile for bash or .zshenv for ZSH).
+case $SHELL in
+*/zsh)
+    PROFILE="${ZDOTDIR-"$HOME"}/.zshenv"
+    PREF_SHELL=zsh
+    ;;
+*/bash)
+    PROFILE=$HOME/.bashrc
+    PREF_SHELL=bash
+    ;;
+*/fish)
+    PROFILE=$HOME/.config/fish/config.fish
+    PREF_SHELL=fish
+    ;;
+*/ash)
+    PROFILE=$HOME/.profile
+    PREF_SHELL=ash
+    ;;
+*)
+    echo "could not detect shell, manually add ${XELIS_DIR} to your PATH."
+    exit 1
+esac
+
+# Only add hvym if it isn't already in PATH.
+if [[ ":$PATH:" != *":${XELIS_DIR}:"* ]]; then
+    # Add the hvym directory to the path and ensure the old PATH variables remain.
+    # If the shell is fish, echo fish_add_path instead of export.
+    if [[ "$PREF_SHELL" == "fish" ]]; then
+        echo >> "$PROFILE" && echo "fish_add_path -a $XELIS_DIR" >> "$PROFILE"
+    else
+        echo >> "$PROFILE" && echo "export PATH=\"\$PATH:$XELIS_DIR\"" >> "$PROFILE"
+    fi
+fi
