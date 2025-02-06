b4ae2124a722e2bf29624132d3f25cef72872179
Jason <dimensions3@outlook.com>
2025-02-06 12:01:04 -0800

    wip

--- install_xelis.sh
+++ install_xelis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing Rust:"
-echo "1\n y\n" | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "1\n" | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo apt install cargo -y
 git clone https://github.com/xelis-project/xelis-blockchain
 source ~/.profile
@@ -11,5 +11,6 @@ cargo build --release
 mkdir -p /home/.local/share/xelis-blockchain
 
 mv /home/test/xelis-blockchain/target/release/* /home/.local/share/xelis-blockchain/
-export PATH="/home/.local/share/xelis-blockchain/:$PATH"
+echo "export xelis to path:"
+sudo export PATH="/home/.local/share/xelis-blockchain/:$PATH"
 echo "Xelis is installed."
