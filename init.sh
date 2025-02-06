2fe34c80a8119b8bb02fee25df231512c6788883
Jason <dimensions3@outlook.com>
2025-02-05 18:07:42 -0800

    wip

--- init.sh
+++ init.sh
@@ -7,6 +7,9 @@ sudo apt install python3-dev -y
 sudo apt install python3-pip -y
 sudo apt install python3.12-venv -y
 
+echo "Installing Rust:"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 echo "Cloning axiel"
 git clone https://github.com/inviti8/axiel.git
 echo "Creating Env"
@@ -19,6 +22,14 @@ echo "Installing requirements:"
 pip install -r /home/test/axiel/requirements.txt
 echo "Deactivate the environment"
 deactivate
-chmod +x axiel/setup.sh
 usermod -a -G test www-data
+echo "refresh the shell"
+source ~/.profile
+echo "Installing xelis"
+git clone https://github.com/xelis-project/xelis-blockchain
+echo "Building the blockchain"
+cargo build /home/test/xelis-blockchain/ --release
+echo "Installing axiel"
+cd /home/test/axiel && python3 setup.py install
+chmod +x axiel/setup.sh
 ./axiel/setup.sh
