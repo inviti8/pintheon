440c16313aee941512745bf5ebd84957fb14ae0b
Jason <dimensions3@outlook.com>
2025-02-07 15:49:07 -0800

    fix

--- install_xelis.sh
+++ install_xelis.sh
@@ -4,7 +4,7 @@ DIR="/home/.local/share/xelis-blockchain"
 PROFILE=$(basename $SHELL)rc
 
 echo "export PATH=\"\$PATH:$DIR\"" >> ~/.$PROFILE
-echo "Path should've been updated to include $DIR
+echo "Path should've been updated to include $DIR"
 echo "Installing Rust:"
 curl --proto '=https' -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt install cargo -y
