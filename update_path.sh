3371ff93f8ee027a676bf5567efaabd8cfb7d197
Jason <dimensions3@outlook.com>
2025-02-06 20:07:52 -0800

    OMG updating the path is a !!!

--- update_path.sh
+++ update_path.sh
@@ -1,40 +1,6 @@
-#!/usr/bin/bash
-set -eo pipefail
-echo "Updating path:"
-BASE_DIR="${XDG_CONFIG_HOME:-$HOME}"
-LOCAL_DIR="${LOCAL_DIR-"$BASE_DIR/.local"}"
-XELIS_DIR="$LOCAL_DIR/share/xelis-blockchain"
+#!/bin/bash
+DIR="/home/.local/share/xelis-blockchain"
 
-# Store the correct profile file (i.e. .profile for bash or .zshenv for ZSH).
-case $SHELL in
-*/zsh)
-    PROFILE="${ZDOTDIR-"$HOME"}/.zshenv"
-    PREF_SHELL=zsh
-    ;;
-*/bash)
-    PROFILE=$HOME/.bashrc
-    PREF_SHELL=bash
-    ;;
-*/fish)
-    PROFILE=$HOME/.config/fish/config.fish
-    PREF_SHELL=fish
-    ;;
-*/ash)
-    PROFILE=$HOME/.profile
-    PREF_SHELL=ash
-    ;;
-*)
-    echo "could not detect shell, manually add ${PRESS_DIR} to your PATH."
-    exit 1
-esac
+PROFILE=$(basename $SHELL)rc
 
-# Only add hvym if it isn't already in PATH.
-if [[ ":$PATH:" != *":${XELIS_DIR}:"* ]]; then
-    # Add the hvym directory to the path and ensure the old PATH variables remain.
-    # If the shell is fish, echo fish_add_path instead of export.
-    if [[ "$PREF_SHELL" == "fish" ]]; then
-        echo >> "$PROFILE" && echo "fish_add_path -a $XELIS_DIR" >> "$PROFILE"
-    else
-        echo >> "$PROFILE" && echo "export PATH=\"\$PATH:$XELIS_DIR\"" >> "$PROFILE"
-    fi
-fi
+echo "export PATH=\"\$PATH:$DIR\"" >> ~/.$PROFILE
