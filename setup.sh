3ac96908d16315a9e6cf942b45fedf4ed0eca1f1
Jason <dimensions3@outlook.com>
2025-05-07 17:05:58 -0700

    fix nginx config generation?

--- setup.sh
+++ setup.sh
@@ -34,10 +34,10 @@ server{
 
     location / {
         proxy_pass http://127.0.0.1:8082;
-        proxy_set_header Host `echo $host`;
-        proxy_set_header X-Real-IP `echo $remote_addr`;
-        proxy_set_header X-Forwarded-For `echo $proxy_add_x_forwarded_for`;
-        proxy_set_header X-Forwarded-Proto `echo $scheme`;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
 	}
 
     location /admin {
