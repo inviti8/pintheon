045e9552019a45c2b050af31511043943b5994b2
Jason <dimensions3@outlook.com>
2025-05-10 15:12:13 -0700

    hook up https WIP

--- setup.sh
+++ setup.sh
@@ -27,12 +27,33 @@ echo "Installing nginx:"
 sudo apt install nginx -y
 echo "Configuring Nginx"
 
+echo "Generate SSL Certs."
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/localhost.key -out certs/localhost.crt -config ./localhost.cnf -extensions ext
+
 cat > /etc/nginx/sites-available/default<<  EOF
 
 server{
     listen 80;
     server_name localhost;
     client_max_body_size 200M;
+
+    location / {
+        return 301 https://$host$request_uri;
+     }
+}
+
+server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+    server_name localhost;
+    client_max_body_size 200M;
+
+    http2 on;
+
+    ssl_certificate /etc/ssl/localhost.crt;
+    ssl_certificate_key /etc/ssl/localhost.key;
+    
+    ssl_protocols TLSv1.2 TLSv1.1 TLSv1;
 
     location ~ ^/(ipfs|ipns) {
         proxy_pass http://127.0.0.1:8082;
