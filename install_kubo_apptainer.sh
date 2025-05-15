edc9d67577c52425a6525c988606437f99cbc088
Jason <dimensions3@outlook.com>
2025-05-15 14:18:15 -0700

    clean up

--- install_kubo_apptainer.sh
+++ install_kubo_apptainer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /home
+cd /root
 echo "ipfs install"
-# . "$HOME/.bash_profile"
 echo 'sudo apt update'
 apt update -y
 
@@ -12,25 +11,22 @@ export SWARM_KEY=$(tr -dc a-f0-9 </dev/urandom | head -c 64; echo '')
 echo "Created secret: $SWARM_KEY"
 
 export LIBP2P_FORCE_PNET=1
-echo 'export IPFS_PATH=/.ipfs' >>/home
+echo 'export IPFS_PATH=/.ipfs' >>~/.profile
 source ~/.profile
 
 echo "IPFS_PATH:"
 echo "$IPFS_PATH"
 
 ver="v0.31.0" 
-echo 'wget https://dist.ipfs.tech/kubo/v0.31.0/kubo_v0.31.0_linux-amd64.tar.gz'
-# wget https://dist.ipfs.tech/kubo/v0.31.0/kubo_v0.31.0_linux-amd64.tar.gz
+echo 'wget https://dist.ipfs.tech/kubo/v0.34.1/kubo_v0.34.1_linux-amd64.tar.gz'
 wget https://dist.ipfs.tech/kubo/v0.34.1/kubo_v0.34.1_linux-amd64.tar.gz
 
-echo 'tar -xvzf kubo_v0.31.0_linux-amd64.tar.gz'
-# tar -xvzf kubo_v0.31.0_linux-amd64.tar.gz
+echo 'tar -xvzf kubo_v0.34.1_linux-amd64.tar.gz'
 tar -xvzf kubo_v0.34.1_linux-amd64.tar.gz
 
 echo './kubo/install.sh'
 ./kubo/install.sh
 
-#sudo bash $HOME/kubo/install.sh
 ipfs --version
 
 mkdir -p $IPFS_PATH
@@ -63,34 +59,5 @@ ipfs config Addresses.Gateway /ip4/127.0.0.1/tcp/8082
 echo 'ipfs config Gateway.PublicGateways '
 ipfs config Gateway.PublicGateways --json '{"localhost": null }'
 
-#SETUP IPFS AS SERVICE
-# echo "Creating Kubo service"
-# cat > /etc/systemd/system/ipfs.service <<  EOF
-# [Unit]
-# Description=Kubo Service
-# After=syslog.target network.target remote-fs.target nss-lookup.target
-
-# [Service]
-# User=test
-# WorkingDirectory=$IPFS_PATH
-# ExecStart=/usr/local/bin/ipfs daemon
-# Restart=always
-
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-
-# echo "ipfs sevice created."
-
 sudo chown -R root $IPFS_PATH
 source ~/.profile
-
-# sudo systemctl daemon-reload
-# echo "systemctl daemon reloaded."
-
-# sudo systemctl enable ipfs
-# service ipfs on
-# echo "service ipfs on"
-
-# service ipfs start
-# echo "service ipfs start"
--- setup_apptainer.sh
+++ setup_apptainer.sh
@@ -1,30 +1,4 @@
 #!/bin/sh
-# echo "Creating gunicorn service"
-# cat > /etc/systemd/system/gunicorn.service <<  EOF
-# [Unit]
-# Description=Gunicorn Service
-# After=network.target
-
-# [Service]
-# User=test
-# Group=www-data
-# WorkingDirectory=/philos
-# Environment="PATH=/philos/bin"
-# ExecStart=/philos/philos/bin/gunicorn --workers 1 --limit-request-line 0 --bind unix:philos.sock -m 007 wsgi:app
-# Restart=always
-
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-
-# echo "Starting Gunicorn Service..."
-# # systemctl daemon-reload
-# # systemctl enable gunicorn.service
-# chkconfig gunicorn on
-# # systemctl start gunicorn.service
-# service gunicorn start
-
-
 echo "Installing nginx:"
 apt install nginx -y
 echo "Configuring Nginx"
@@ -34,8 +8,6 @@ mkcert -install
 mkcert local.philos.com localhost 127.0.0.1 ::1
 mv -f local.philos.com+3.pem /etc/ssl/philos.crt
 mv -f local.philos.com+3-key.pem /etc/ssl/philos.key
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/localhost.key -out certs/localhost.crt -config ./philos/localhost.cnf -extensions ext
-#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt -config ./philos/localhost.cnf
 
 cat > /etc/nginx/sites-available/default<<  EOF
 
@@ -87,6 +59,4 @@ echo "Owning the directory"
 chown -R root:www-data /home/philos/
 echo "set ownership to nginx for staic files"
 chmod -R 755 /home/philos/static
-echo "Restarting Nginx"
-# systemctl restart nginx
-# service nginx restart
+
